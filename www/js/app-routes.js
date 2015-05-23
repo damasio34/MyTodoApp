57f82cf693d91e7979b36804fcf1b389f4642947
damasio34 <damasio34@gmail.com>
2015-05-23 18:59:41 -0300

    Implementação das telas e serviços

---
+++ www/js/app-routes.js
@@ -0,0 +1,20 @@
+angular.module('MyTodoApp').config(function($stateProvider){
+
+        $stateProvider.state('todos',{
+            url:'/todos',
+            controller:'TodoListController',
+            templateUrl:'views/todos.html'
+        })
+        .state('createTodo',{
+            url:'/todo/new',
+            controller:'TodoCreationController',
+            templateUrl:'views/create-todo.html'
+        })
+        .state('editTodo',{
+            url:'/todo/edit/:id/:content',
+            controller:'TodoEditController',
+            templateUrl:'views/edit-todo.html'
+        });
+
+        $urlRouterProvider.otherwise('/todos');
+}); 
